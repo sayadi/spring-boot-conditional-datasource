577c793cf15a83f1abf6f349422b629d2b7a6f17
Sayadi <muhammed.alsayadi@gmail.com>
2019-08-11 15:13:46 +0400

    Initial commit

---
+++ src/main/java/io/sayadi/conditionaldatasource/ConditionalDatasourceApplication.java
@@ -0,0 +1,13 @@
+package io.sayadi.conditionaldatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalDatasourceApplication.class, args);
+	}
+
+}
